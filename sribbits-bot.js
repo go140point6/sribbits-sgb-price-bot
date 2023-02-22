99dac3f834d0f46220b575cb2ae92e6582f00829
go140point6 <50885069+go140point6@users.noreply.github.com>
2023-02-22 09:32:34 -0800

    update template for sribbits

--- sribbits-bot.js
+++ sribbits-bot.js
@@ -162,8 +162,8 @@ async function getInitialPrice() {
 
   const WSB_ADDRESS = "0x02f0826ef6ad107cfc861152b32b52fd11bab9ed"
   const wsb = "wsb"
-  const SPRK_ADDRESS = "0xfd2a0fD402828fDB86F9a9D5a760242AD7526cC0"
-  const sprk = "sprk"
+  const sribbits_ADDRESS = "0x399e279c814a3100065fceab8cba1ab114805344"
+  const sribbits = "sribbits"
 
   //let sgbBN = await ftsoRegistryContract["getCurrentPrice(string)"]("SGB")
   //let sgb = Number(sgbBN._price) / 10 ** 5;
@@ -171,31 +171,31 @@ async function getInitialPrice() {
 
   let arryPrice = await ftsoContract.getAmountsOut(
       ethers.utils.parseEther("1"),
-      [WSB_ADDRESS, SPRK_ADDRESS]
+      [WSB_ADDRESS, sribbits_ADDRESS]
   )
 
   //console.log(arryPrice[0])
   //console.log(arryPrice[1])
 
   let wsbSGBPrice = Number(arryPrice[0]._hex) / 10 ** 18
-  let sprkSGBPrice = (Number(arryPrice[1]._hex) / 10 ** 18).toFixed(5)
+  let sribbitsSGBPrice = (Number(arryPrice[1]._hex) / 10 ** 18).toFixed(5)
   let wsbUSDPrice = (sgbPrice / wsbSGBPrice).toFixed(5)
-  let sprkUSDPrice = (sgbPrice / sprkSGBPrice).toFixed(5)
+  let sribbitsUSDPrice = (sgbPrice / sribbitsSGBPrice).toFixed(5)
   console.log(wsb + " USD price is " + wsbUSDPrice)
-  console.log(sprk + " USD price is " + sprkUSDPrice)
+  console.log(sribbits + " USD price is " + sribbitsUSDPrice)
 
   clearRoles()
-  lastPrice = sprkUSDPrice || 0
+  lastPrice = sribbitsUSDPrice || 0
   let symbol = `${process.env.COIN_ID.toUpperCase()}`
   client.user.setPresence({
     activities: [{
-    name: `SGB=${sprkSGBPrice} ${symbol}`,
+    name: `SGB=${sribbitsSGBPrice} ${symbol}`,
     type: `PLAYING`
     }]
   })
 
   arrow = mid
-  client.guilds.cache.find(guild => guild.id === process.env.SERVER_ID).me.setNickname(`${symbol} ${arrow} ${process.env.CURRENCY_SYMBOL}${sprkUSDPrice}`)
+  client.guilds.cache.find(guild => guild.id === process.env.SERVER_ID).me.setNickname(`${symbol} ${arrow} ${process.env.CURRENCY_SYMBOL}${sribbitsUSDPrice}`)
   
   console.log('Initial price to', lastPrice)
   //console.log(`SGB: ${sgbPrice} per ${symbol}`)
@@ -246,8 +246,8 @@ async function getPrices() {
 
   const WSB_ADDRESS = "0x02f0826ef6ad107cfc861152b32b52fd11bab9ed"
   const wsb = "wsb"
-  const SPRK_ADDRESS = "0xfd2a0fD402828fDB86F9a9D5a760242AD7526cC0"
-  const sprk = "sprk"
+  const sribbits_ADDRESS = "0x399e279c814a3100065fceab8cba1ab114805344"
+  const sribbits = "sribbits"
 
   //let sgbBN = await ftsoRegistryContract["getCurrentPrice(string)"]("SGB")
   //let sgb = Number(sgbBN._price) / 10 ** 5;
@@ -255,25 +255,25 @@ async function getPrices() {
 
   let arryPrice = await ftsoContract.getAmountsOut(
       ethers.utils.parseEther("1"),
-      [WSB_ADDRESS, SPRK_ADDRESS]
+      [WSB_ADDRESS, sribbits_ADDRESS]
   )
 
   //console.log(arryPrice[0])
   //console.log(arryPrice[1])
 
   let wsbSGBPrice = Number(arryPrice[0]._hex) / 10 ** 18
-  let sprkSGBPrice = (Number(arryPrice[1]._hex) / 10 ** 18).toFixed(5)
+  let sribbitsSGBPrice = (Number(arryPrice[1]._hex) / 10 ** 18).toFixed(5)
   let wsbUSDPrice = (sgbPrice / wsbSGBPrice).toFixed(5)
-  let sprkUSDPrice = (sgbPrice / sprkSGBPrice).toFixed(5)
+  let sribbitsUSDPrice = (sgbPrice / sribbitsSGBPrice).toFixed(5)
   console.log(wsb + " USD price is " + wsbUSDPrice)
-  console.log(sprk + " USD price is " + sprkUSDPrice)
+  console.log(sribbits + " USD price is " + sribbitsUSDPrice)
 
-  currentPrice = sprkUSDPrice || 0
+  currentPrice = sribbitsUSDPrice || 0
   let symbol = `${process.env.COIN_ID.toUpperCase()}`
   client.user.setPresence({
     activities: [{
     //name: `SGB: ${sgbPrice} per ${symbol}`,
-    name: `SGB=${sprkSGBPrice} ${symbol}`,
+    name: `SGB=${sribbitsSGBPrice} ${symbol}`,
     type: `PLAYING`
     }]
   })
@@ -292,7 +292,7 @@ async function getPrices() {
         console.log('same')
       }
 
-  client.guilds.cache.find(guild => guild.id === process.env.SERVER_ID).me.setNickname(`${symbol} ${arrow} ${process.env.CURRENCY_SYMBOL}${sprkUSDPrice}`)
+  client.guilds.cache.find(guild => guild.id === process.env.SERVER_ID).me.setNickname(`${symbol} ${arrow} ${process.env.CURRENCY_SYMBOL}${sribbitsUSDPrice}`)
 
   //console.log('Current price to', currentPrice)
   //console.log(`SGB: ${sgbPrice} per ${symbol}`)
